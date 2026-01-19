e277fc490f55528c354c2164f41f2b18b1e3459e
Nathan Moos <moosingin3space@gmail.com>
2026-01-18 16:05:24 -0800

    feat: add initial Rust module

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Daggerverse Modules
+
+These are a few modules I'm finding incredibly useful as part of my personal projects.
---
+++ modules/rust/main.go
@@ -0,0 +1,83 @@
+// A set of functions that can be used to build Rust easily.
+
+package main
+
+import (
+	"context"
+	"dagger/rust/internal/dagger"
+)
+
+type Rust struct {
+	*dagger.Container
+	source *dagger.Directory
+}
+
+// Creates a Rust development environment.
+func (m *Rust) New(
+	//+defaultPath="/rust-toolchain.toml"
+	toolchainFile *dagger.File,
+	//+defaultPath="/"
+	source *dagger.Directory,
+	//+optional
+	extraPackages []string,
+) *Rust {
+	packages := []string{
+		"rustup",
+		"build-base",
+		"openssl-dev",
+		"pkgconf",
+		"curl",
+	}
+	if extraPackages != nil {
+		packages = append(packages, extraPackages...)
+	}
+	// TODO try not to hardcode
+	// TODO key these caches appropriately
+	ctr := dag.Wolfi().
+		Container(dagger.WolfiContainerOpts{
+			Packages: packages,
+		}).
+		WithEnvVariable("CARGO_HOME", "/usr/local/cargo").
+		WithEnvVariable("RUSTUP_HOME", "/usr/local/rustup").
+		WithMountedCache("/usr/local/cargo", dag.CacheVolume("cargo-home")).
+		WithMountedCache("/usr/local/rustup", dag.CacheVolume("rustup-home")).
+		WithExec([]string{"rustup-init", "-y", "--default-toolchain", "none"}).
+		WithEnvVariable("PATH", "/usr/local/cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin").
+		WithWorkdir("/src")
+	if toolchainFile != nil {
+		ctr = ctr.WithMountedFile("/src/rust-toolchain.toml", toolchainFile).
+			WithExec([]string{"rustup", "toolchain", "install"})
+	} else {
+		// Default to the latest stable toolchain
+		ctr = ctr.WithExec([]string{"rustup", "toolchain", "install", "stable"})
+	}
+	ctr = ctr.WithMountedDirectory("/src", source).
+		WithMountedCache("/src/target", dag.CacheVolume("target"))
+	return &Rust{
+		ctr,
+		source,
+	}
+}
+
+func (m *Rust) CargoCheck(
+	ctx context.Context,
+) (string, error) {
+	return m.Container.
+		WithExec([]string{"cargo", "check", "--all", "--all-targets"}).
+		Stdout(ctx)
+}
+
+func (m *Rust) CargoFmtCheck(
+	ctx context.Context,
+) (string, error) {
+	return m.Container.
+		WithExec([]string{"cargo", "fmt", "--all", "--check"}).
+		Stdout(ctx)
+}
+
+func (m *Rust) CargoFmtFix() *dagger.Changeset {
+	generated := m.Container.
+		WithExec([]string{"cargo", "fmt", "--all"}).
+		Directory("/src")
+	return generated.Changes(m.source)
+}
